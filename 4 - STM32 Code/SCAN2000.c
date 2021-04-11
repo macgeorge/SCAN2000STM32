4f18c357a4724e9321dc98c93b862ff93bf7efd0
macgorge <macgeorge@users.noreply.github.com>
2021-04-11 19:53:48 +0100

    Updated include directives
    Removed main.h reference and added include directives to STM32 libraries

--- 4 - STM32 Code/SCAN2000.c
+++ 4 - STM32 Code/SCAN2000.c
@@ -1,6 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include "stm32g0xx_ll_rcc.h"
+#include "stm32g0xx_ll_bus.h"
+#include "stm32g0xx_ll_system.h"
+#include "stm32g0xx_ll_exti.h"
+#include "stm32g0xx_ll_cortex.h"
+#include "stm32g0xx_ll_utils.h"
+#include "stm32g0xx_ll_pwr.h"
+#include "stm32g0xx_ll_dma.h"
+#include "stm32g0xx_ll_usart.h"
+#include "stm32g0xx_ll_gpio.h"
 
 #define MAXALLOWEDONCHANNELS 6
 
